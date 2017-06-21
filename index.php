4cd9819c0be0aa06df6f1cdeabb1b255ec079d53
Berzeg07 <berezg86@gmail.com>
2017-06-21 21:37:40 +0400

    21.06

---
+++ index.php
@@ -0,0 +1,136 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+	<title>Разработка сайтов в Нальчике</title>
+
+	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
+	<link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" />
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="libs/owl.carousel/assets/owl.carousel.css" />
+
+	
+</head>
+<body>
+
+	<div class="header">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+					<div class="header__box">
+						<img class="img-responsive" src="img/logo.png" alt="">
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+					<div class="header__box">
+						<div class="header__box_title">
+							<h1>Разработка сайтов в Нальчике</h1>
+							<span>Сайты под ключ, веб дизайн, сео продвижение</span>
+						</div>
+						<div class="header__box_menu">
+							<div class="bars">
+								<i class="fa fa-bars" aria-hidden="true"></i>
+							</div>
+							<ul class="nav">
+								<li><a href="#">Главная</a></li>
+								<li><a href="#">Услуги</a></li>
+								<li><a href="#">Портфолио</a></li>
+								<li><a href="#">Контакты</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+					<div class="header__box header__box_tel">
+						<span>+7(928)-700-50-86</span><br/>
+						<a href="#">Заказать звонок</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<!-- Slider Header -->
+	<div class="slider-wrap">
+		<div id="slider">
+
+			<div class="item">
+				<div class="item__box">
+					<img class="img-responsive" src="img/sl1.jpg" alt="">
+				</div>
+			</div>	
+
+			<div class="item">
+				<div class="item__box">
+					<img class="img-responsive" src="img/sl1.jpg" alt="">
+				</div>
+			</div>	
+
+			<div class="item">
+				<div class="item__box">
+					<img class="img-responsive" src="img/sl1.jpg" alt="">
+				</div>
+			</div>	
+
+			<div class="item">
+				<div class="item__box">
+					<img class="img-responsive" src="img/sl1.jpg" alt="">
+				</div>
+			</div>	
+
+			<div class="item">
+				<div class="item__box">
+					<img class="img-responsive" src="img/sl1.jpg" alt="">
+				</div>
+			</div>	
+
+		</div>
+	</div>
+	<!-- End Slider Header -->
+	<div class="uslugi-wrap">
+		<div class="container">
+			<h2>Услуги</h2>
+			<div class="row">
+
+				<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+					<div class="uslugi-box uslugi-box__1">
+						<span>Разработка сайтов</span>
+					</div>
+					<div class="uslugi-box uslugi-box__2">
+						<span>Продвижение сайтов</span>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+					<div class="uslugi-box uslugi-box__3">
+						<span>Веб дизайн</span>
+					</div>
+					<div class="uslugi-box uslugi-box__4">
+						<span>Интернет маркетинг</span>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 hidden-xs hidden-sm"></div>
+
+			</div>
+		</div>
+	</div>
+
+
+
+
+
+
+
+
+
+	<!-- JS_BLOCK -->
+	<script src="libs/jquery/jquery-1.11.1.min.js"></script>
+	<script src="libs/owl.carousel/owl.carousel.js"></script>
+	<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
+	<script src="js/jquery.maskedinput.min.js"></script>
+	<script src="js/fonts.js"></script>
+	<script src="js/common.js"></script>
+
+</body>
+</html>
---
+++ js/common.js
@@ -0,0 +1,94 @@
+$(document).ready(function(){
+
+	// Мобильное меню 
+	$(".fa-bars").click(function(m){
+		m.preventDefault();
+		$(".nav").slideToggle(500);
+	});
+	// Конец Мобильное меню
+
+   var owl2 = $("#slider");
+   owl2.owlCarousel({
+    loop:true,
+    nav:true, 
+    autoplay:true,
+    smartSpeed:1000,
+    margin:0,
+        center:false,     //если нужны обрезаные края
+        navText:['<span class="nav-left"></span>','<span class="nav-right"></span>'],
+        responsive:{
+            0:{
+                items:1
+            },    
+        }
+    });
+
+    // Fancybox
+    $("a[rel=group]").fancybox({
+        'transitionIn' : 'none',
+        'transitionOut' : 'none',
+        'titlePosition' : 'over',
+        'titleFormat' : function(title, currentArray, currentIndex, currentOpts) {
+            return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
+        }
+    });
+
+    // Modal window
+    $('a[name=modal]').click(function(e) {
+    	e.preventDefault();
+    	var id = $(this).attr('href');
+    	var maskHeight = $(document).height();
+    	var maskWidth = $(window).width();
+    	$('#mask').css({'width':maskWidth,'height':maskHeight});
+    	$('#mask').fadeTo("slow",0.8); 
+    	var winH = $(window).height();
+    	var winW = $(window).width();
+    	posTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement ||document.body.parentNode || document.body).scrollTop;
+    	$(id).css('top',  posTop+50);
+    	$(id).css('left', winW/2-$(id).width()/2);
+    	$(id).fadeIn(500); 
+    });
+    $('.window .dd-close').click(function (e) {
+    	e.preventDefault();
+    	$('#mask, .window').hide();
+    	$('.window').hide();
+    }); 
+
+    $('#mask, .an-exit__krest').click(function () {
+    	$('#mask').hide();
+    	$('.window').hide();
+    }); 
+    $(".phone").mask("+ 7 (999) 999 - 99 - 99?"); 
+    $(".form-message").submit(function() { 
+    	var tel = $(this).find('input[name="phone"]');
+    	var empty = false;
+    	if (tel.val() == ""){
+    		empty = true;
+    	}
+    	if (empty == true){
+    		tel.addClass("error-input");
+    		tel.focus();
+    	}else{
+    		var form_data = $(this).serialize(); 
+    		$.ajax({
+    			type: "POST", 
+    			url: "/message.php", 
+    			data: form_data,
+    			success: function() {
+    				cleanTnanks(this);
+    			}
+    		});
+    	}
+    	return false;
+    }); 
+
+    function cleanTnanks(form){
+    	$('input[type="text"]').removeClass("error-input");
+    	$("input[type=text], textarea").val("");
+    	$('.window').hide();
+    	$('a[href=#thanks]').trigger('click');
+
+    };
+
+
+});//END READY
